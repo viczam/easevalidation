9557fbd1ea259e5a789de0ccb819ec80a04be7c5
Victor Zamfir <zamfir.victor@gmail.com>
2018-11-29 02:17:36 +0200

    `test` uses `validate`

--- src/test.js
+++ src/test.js
@@ -1,31 +1,19 @@
+import validate from './validate';
 import { argsToValidators } from './helpers';
 
 export default (...args) => {
   const validators = argsToValidators(args);
-  const validate = initialValue => {
-    let value = initialValue;
-    let error;
-
-    const result = validators.reduce((isValid, validator) => {
-      if (!isValid) {
-        return isValid;
-      }
-
-      try {
-        value = validator(value);
-        error = null;
-        return true;
-      } catch (err) {
-        error = err;
-        return false;
-      }
-    }, true);
-
-    validate.value = value;
-    validate.error = error;
-
-    return result;
+  const doValidate = initialValue => {
+    try {
+      const value = validate(validators)(initialValue);
+      doValidate.value = value;
+      doValidate.error = undefined;
+      return true;
+    } catch (error) {
+      doValidate.value = undefined;
+      doValidate.error = error;
+      return false;
+    }
   };
-
-  return validate;
+  return doValidate;
 };
