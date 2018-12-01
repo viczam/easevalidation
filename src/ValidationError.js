5a6080e5e67577a743c34a38151cd279e27f9d91
Victor Zamfir <zamfir.victor@gmail.com>
2018-12-01 10:13:18 +0200

    ValidationError can accept an optional message

--- src/ValidationError.js
+++ src/ValidationError.js
@@ -1,11 +1,13 @@
 class ValidationError extends Error {
-  constructor({ value, code, config, error }) {
+  constructor({ value, code, config, error, message }) {
     super();
     this.name = this.constructor.name;
     Error.captureStackTrace(this, this.constructor);
-    this.message = `Failed passing "${code}" validator!${
-      error && error instanceof Error ? ` (${error.message || error.toString()})` : ''
-    }`;
+    this.message =
+      message ||
+      `Failed passing "${code}" validator!${
+        error && error instanceof Error ? ` (${error.message || error.toString()})` : ''
+      }`;
     this.value = value;
     this.code = code;
     this.config = config;
