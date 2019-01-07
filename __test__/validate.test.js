9a9811d777a23e91cd6030ac48a92c5f17df2a53
Victor Zamfir <zamfir.victor@gmail.com>
2019-01-07 11:47:31 +0200

    more tests - "validate" is composable

--- __test__/validate.test.js
+++ __test__/validate.test.js
@@ -13,4 +13,16 @@ describe('validate', () => {
     expect(() => validate(v.isNumber())('2')).not.toThrow();
     expect(validate(v.isNumber())('2')).toBe(2);
   });
+
+  it('is composable', () => {
+    expect(() => validate(validate(v.isNumber()))('abc')).toThrow();
+    expect(() => validate(validate(v.isNumber()), validate(v.isMin(1)))(2)).not.toThrow();
+    expect(() =>
+      validate(validate(v.isNumber()), validate(validate(v.isMin(3), v.isMax(5))))(2),
+    ).toThrow('isMin');
+    expect(() =>
+      validate(validate(v.isNumber()), validate(validate(v.isMin(3)), v.isMax(5)))(6),
+    ).toThrow('isMax');
+    expect(() => validate(validate(v.isNumber()), validate(v.isMin(3)))('abc')).toThrow('isNumber');
+  });
 });
