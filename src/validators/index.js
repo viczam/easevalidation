7a4bf26c331f011a33903875bad5b65622d30b53
Victor Zamfir <zamfir.victor@gmail.com>
2019-08-29 16:42:47 +0300

    direct imports from validator.js

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-cycle */
-import pick from 'lodash/pick';
 import isArguments from 'lodash/isArguments';
 import isArrayBuffer from 'lodash/isArrayBuffer';
 import isArrayLike from 'lodash/isArrayLike';
@@ -30,7 +29,54 @@ import isTypedArray from 'lodash/isTypedArray';
 import isUndefined from 'lodash/isUndefined';
 import isWeakMap from 'lodash/isWeakMap';
 import isWeakSet from 'lodash/isWeakSet';
-import validatorsJs from 'validator';
+import matches from 'validator/lib/matches';
+import isAlphanumeric from 'validator/lib/isAlphanumeric';
+import isAlpha from 'validator/lib/isAlpha';
+import isAscii from 'validator/lib/isAscii';
+import isBase64 from 'validator/lib/isBase64';
+import isByteLength from 'validator/lib/isByteLength';
+import isCreditCard from 'validator/lib/isCreditCard';
+import isCurrency from 'validator/lib/isCurrency';
+import isDataURI from 'validator/lib/isDataURI';
+import isDecimal from 'validator/lib/isDecimal';
+import isEmail from 'validator/lib/isEmail';
+import isFQDN from 'validator/lib/isFQDN';
+import isFullWidth from 'validator/lib/isFullWidth';
+import isHalfWidth from 'validator/lib/isHalfWidth';
+import isHash from 'validator/lib/isHash';
+import isHexColor from 'validator/lib/isHexColor';
+import isHexadecimal from 'validator/lib/isHexadecimal';
+import isIP from 'validator/lib/isIP';
+import isIPRange from 'validator/lib/isIPRange';
+import isISBN from 'validator/lib/isISBN';
+import isISIN from 'validator/lib/isISIN';
+import isISO31661Alpha2 from 'validator/lib/isISO31661Alpha2';
+import isISO31661Alpha3 from 'validator/lib/isISO31661Alpha3';
+import isISO8601 from 'validator/lib/isISO8601';
+import isISRC from 'validator/lib/isISRC';
+import isISSN from 'validator/lib/isISSN';
+import isIdentityCard from 'validator/lib/isIdentityCard';
+import isJSON from 'validator/lib/isJSON';
+import isJWT from 'validator/lib/isJWT';
+import isLatLong from 'validator/lib/isLatLong';
+import isLowercase from 'validator/lib/isLowercase';
+import isMACAddress from 'validator/lib/isMACAddress';
+import isMD5 from 'validator/lib/isMD5';
+import isMagnetURI from 'validator/lib/isMagnetURI';
+import isMimeType from 'validator/lib/isMimeType';
+import isMobilePhone from 'validator/lib/isMobilePhone';
+import isMultibyte from 'validator/lib/isMultibyte';
+import isNumeric from 'validator/lib/isNumeric';
+import isPort from 'validator/lib/isPort';
+import isPostalCode from 'validator/lib/isPostalCode';
+import isRFC3339 from 'validator/lib/isRFC3339';
+import isSurrogatePair from 'validator/lib/isSurrogatePair';
+import isURL from 'validator/lib/isURL';
+import isUUID from 'validator/lib/isUUID';
+import isUppercase from 'validator/lib/isUppercase';
+import isVariableWidth from 'validator/lib/isVariableWidth';
+import isWhitelisted from 'validator/lib/isWhitelisted';
+import isMongoId from 'validator/lib/isMongoId';
 import isString from './isString';
 import isNumber from './isNumber';
 import isAny from './isAny';
@@ -95,56 +141,54 @@ export default {
     isUndefined,
     isWeakMap,
     isWeakSet,
-    isRegExpMatch: validatorsJs.matches,
-    ...pick(validatorsJs, [
-      'isAlphanumeric',
-      'isAlpha',
-      'isAscii',
-      'isBase64',
-      'isByteLength',
-      'isCreditCard',
-      'isCurrency',
-      'isDataURI',
-      'isDecimal',
-      'isEmail',
-      'isFQDN',
-      'isFullWidth',
-      'isHalfWidth',
-      'isHash',
-      'isHexColor',
-      'isHexadecimal',
-      'isIP',
-      'isIPRange',
-      'isISBN',
-      'isISIN',
-      'isISO31661Alpha2',
-      'isISO31661Alpha3',
-      'isISO8601',
-      'isISRC',
-      'isISSN',
-      'isIdentityCard',
-      'isJSON',
-      'isJWT',
-      'isLatLong',
-      'isLowercase',
-      'isMACAddress',
-      'isMD5',
-      'isMagnetURI',
-      'isMimeType',
-      'isMobilePhone',
-      'isMultibyte',
-      'isNumeric',
-      'isPort',
-      'isPostalCode',
-      'isRFC3339',
-      'isSurrogatePair',
-      'isURL',
-      'isUUID',
-      'isUppercase',
-      'isVariableWidth',
-      'isWhitelisted',
-      'isMongoId',
-    ]),
+    isRegExpMatch: matches,
+    isAlphanumeric,
+    isAlpha,
+    isAscii,
+    isBase64,
+    isByteLength,
+    isCreditCard,
+    isCurrency,
+    isDataURI,
+    isDecimal,
+    isEmail,
+    isFQDN,
+    isFullWidth,
+    isHalfWidth,
+    isHash,
+    isHexColor,
+    isHexadecimal,
+    isIP,
+    isIPRange,
+    isISBN,
+    isISIN,
+    isISO31661Alpha2,
+    isISO31661Alpha3,
+    isISO8601,
+    isISRC,
+    isISSN,
+    isIdentityCard,
+    isJSON,
+    isJWT,
+    isLatLong,
+    isLowercase,
+    isMACAddress,
+    isMD5,
+    isMagnetURI,
+    isMimeType,
+    isMobilePhone,
+    isMultibyte,
+    isNumeric,
+    isPort,
+    isPostalCode,
+    isRFC3339,
+    isSurrogatePair,
+    isURL,
+    isUUID,
+    isUppercase,
+    isVariableWidth,
+    isWhitelisted,
+    isMongoId,
   }),
   isAny,
   isArray,
